00e3ddccd8222f75c79097d21fae6ed087d0b73b
Asadbek_733 <asadbekabdusaidov78@gmail.com>
2023-09-20 02:07:08 +0500

    commit

--- script.js
+++ script.js
@@ -1,24 +1,93 @@
 'use strict';
 
-// document.querySelector(('h1')); 
+// document.querySelector(('h1'));
 
-document.querySelector('.btn01').addEventListener('click', function () {
-    document.querySelector('h1').textContent = "Meni raqamimni top";
-    document.querySelector('.between').textContent = "1 dan 20 gacha";
-    document.querySelector('.again').textContent = "Yana";
-    document.querySelector('.check').textContent = "Tekshiring";
-    document.querySelector('.message').textContent = "Taxmin qilishni boshlang...";
-    document.querySelector('.label-highscore').textContent = "🥇 Yuqori ball: ";
-    document.querySelector('.label-score').textContent = "💯 Ball: ";
+let computerNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+let tili = 'eng';
 
-})
-document.querySelector('.btn02').addEventListener('click', function () {
-    document.querySelector('h1').textContent = "Guess My Number!";
-    document.querySelector('.between').textContent = "Between 1 and 20";
-    document.querySelector('.again').textContent = "Again!";
-    document.querySelector('.check').textContent = "Check!";
-    document.querySelector('.message').textContent = "Start guessing...";
-    document.querySelector('.label-highscore').textContent = "🥇 Highscore: "; 
-    document.querySelector('.label-score').textContent = "💯 Score: ";
+let engToUzb = {
+  '⛔ No number!': '⛔ Raqam yuq',
+  '📈 Too high': '📈 Katta son',
+  '📉 Too low': '📉 Kichik son',
+  '✅ Correct Number': '✅ Tugri son',
+  '😔 You lost the game': '😔 Uyinda yutqazdingiz',
+  'Start guessing...': 'Topishni boshla...',
+};
 
-})
+let english = {
+  '⛔ No number!': '⛔ No number!',
+  '📈 Too high': '📈 Too high',
+  '📉 Too low': '📉 Too low',
+  '✅ Correct Number': '✅ Correct Number',
+  '😔 You lost the game': '😔 You lost the game',
+  'Start guessing...': 'Start guessing...',
+};
+
+function showMessage(message) {
+  if (tili == 'uzb') {
+    document.querySelector('.message').textContent = engToUzb[message];
+  } else {
+    document.querySelector('.message').textContent = english[message];
+  }
+}
+
+// document.querySelector('.number').textContent = computerNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  let guessNumber = Number(document.querySelector('.guess').value);
+
+  if (score > 1) {
+    if (!guessNumber) {
+      showMessage('⛔ No number!');
+    } else if (guessNumber !== computerNumber) {
+      showMessage(guessNumber > computerNumber ? '📈 Too high' : '📉 Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      showMessage('✅ Correct Number');
+      document.querySelector('.number').textContent = computerNumber;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '25rem';
+      highScore = score > highScore ? score : highScore;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else {
+    message.textContent = '😔 You lost the game';
+    document.querySelector('.score').textContent = 0;
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  computerNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.guess').value = '';
+  showMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+document.querySelector('.uzbek').addEventListener('click', function () {
+  tili = 'uzb';
+  document.querySelector('h1').textContent = 'Meni raqamimni top';
+  document.querySelector('.between').textContent = '1 dan 20 gacha';
+  document.querySelector('.again').textContent = 'Yana';
+  document.querySelector('.check').textContent = 'Tekshiring';
+  document.querySelector('.message').textContent = 'Taxminni boshla...';
+  document.querySelector('.label-highscore').innerHTML =
+    '🥇 Yaxshisi: <span class="highscore">0</span>';
+  document.querySelector('.label-score').innerHTML =
+    '💯 Ball: <span class="score">20</span>';
+});
+document.querySelector('.english').addEventListener('click', function () {
+  document.querySelector('h1').textContent = 'Guess My Number!';
+  document.querySelector('.between').textContent = 'Between 1 and 20';
+  document.querySelector('.again').textContent = 'Again!';
+  document.querySelector('.check').textContent = 'Check!';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.label-highscore').textContent = '🥇 Highscore: ';
+  document.querySelector('.label-score').textContent = '💯 Score: ';
+});
